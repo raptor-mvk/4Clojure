1a13733f0c7a47941e12b1fd4091877ba5b515a3
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-28 11:24:13 +0300

    Added task 15

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ Solutions to problems from http://4clojure.com/
 # Topics
 
 * Language
+  * Basic
+	  * [15](http://www.4clojure.com/problem/15),
 	* Basic sequence manipulating
 	  * [19](http://www.4clojure.com/problem/19),
---
+++ src/lang/task15.clj
@@ -0,0 +1,22 @@
+; Topic: language
+; Subtopic: elementary
+
+(ns
+  ^{:author raptor_MVK}
+  lang.task15
+  (:use clojure.test))
+
+(defn x2
+  "Given x, return doubled value of x"
+  [x] (* x 2))
+
+(deftest test1 (is (= (x2 2) 4)))
+
+(deftest test2 (is (= (x2 3) 6)))
+
+(deftest test3 (is (= (x2 11) 22)))
+
+(deftest test4 (is (= (x2 7) 14)))
+
+(run-tests 'lang.task15)
+
