d18cddce5b628a604e71e7d90f4d3c619719c2c1
raptor-mvk <m.v.kamorin@gmail.com>
2015-10-06 12:35:45 +0300

    Added tasks 32, 33

---
+++ src/seqs/task32.clj
@@ -0,0 +1,21 @@
+; Topic: sequences
+
+(ns
+  ^{:author raptor}
+  seqs.task32
+  (:use clojure.test))
+
+(defn dup
+  "Duplicates each element of a sequence"
+  [coll] (interleave coll coll))
+
+(deftest test1 (is (= (dup [1 2 3]) '(1 1 2 2 3 3))))
+
+(deftest test2 (is (= (dup [:a :a :b :b]) '(:a :a :a :a :b :b :b :b))))
+
+(deftest test3 (is (= (dup  [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4]))))
+
+(deftest test4 (is (= (dup [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4]))))
+
+(run-tests 'seqs.task32)
+
---
+++ src/seqs/task33.clj
@@ -0,0 +1,22 @@
+; Topic: sequences
+
+(ns
+  ^{:author raptor}
+  seqs.task33
+  (:use clojure.test))
+
+(defn repl
+  "Given a sequence and a number N, replicates each element of a sequence N times"
+  [coll n] (apply interleave (repeat n coll)))
+
+(deftest test1 (is (= (repl [1 2 3] 2) '(1 1 2 2 3 3))))
+
+(deftest test2 (is (= (repl [:a :b] 4) '(:a :a :a :a :b :b :b :b))))
+
+(deftest test3 (is (= (repl [4 5 6] 1) '(4 5 6))))
+
+(deftest test4 (is (= (repl [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4]))))
+
+(deftest test5 (is (= (repl [44 33] 2) [44 44 33 33])))
+
+(run-tests 'seqs.task33)
